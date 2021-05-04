a03c90bcfe44519c160a39a64fb31325d0a8943f
Diego Haas Sanches <egoforia@gmail.com>
2021-05-04 17:42:22 -0300

    logic to transfer amount from an account to another on after save the transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,4 +4,23 @@ class Transaction < ApplicationRecord
 
   enum status: %i[pending success error], _default: "pending"
   enum transaction_type: %i[transfer], _default: "transfer" # should we add more transaction types (e.g. credit_card)?
+
+  after_save :transfer_balance, if: :pending?
+
+  private
+    def from_account_has_enough_balance?
+      self.from_account.balance >= self.amount
+    end
+
+    def transfer_balance
+      self.transaction do
+        if self.from_account_has_enough_balance?
+          self.from_account.update_attribute(:balance, self.from_account.balance - self.amount)
+          self.to_account.update_attribute(:balance, self.to_account.balance + self.amount)
+          self.success!
+        else
+          self.error!
+        end
+      end
+    end
 end
