9bd1bd647687bccb16b51ad190fe98adb06ab6b3
Diego Haas Sanches <egoforia@gmail.com>
2021-05-04 15:17:18 -0300

    added status and transaction_type enums on transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :from_account, class_name: 'Account'
   belongs_to :to_account, class_name: 'Account'
+
+  enum status: %i[pending success error], _default: "pending"
+  enum transaction_type: %i[transfer], _default: "transfer" # should we add more transaction types (e.g. credit_card)?
 end
