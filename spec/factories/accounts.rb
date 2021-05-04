565a4f38083571bc359ee8a0397384414a0253c5
Diego Haas Sanches <egoforia@gmail.com>
2021-05-04 16:44:29 -0300

    added factories for user and account models

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :account do
-    user { nil }
-    balance { "9.99" }
+    user { build(:user) }
+
+    factory :account_with_balance do
+      transient do
+        balance { 0.0 }
+      end
+
+      before(:create) do |account, evaluator|
+        account.balance = evaluator.balance
+      end
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+
+    factory :user_with_account do
+      after(:create) do |user, evaluator|
+        create(:account, user: user)
+
+        user.reload
+      end
+    end
   end
 end
