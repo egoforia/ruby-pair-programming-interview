04979e8a79ed4ec61aa39aec3a656f997e851b01
Diego Haas Sanches <egoforia@gmail.com>
2021-05-04 15:39:42 -0300

    added test to verify that account have many transactions with foreign key from_account_id

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   describe 'associations' do
     it { should belong_to(:user)  }
+    it { should have_many(:transactions).with_foreign_key('from_account_id') }
   end
 end
