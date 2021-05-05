07f6f42ef6e653c5c65fb703dba750d8438b817d
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 16:25:40 -0300

    added validations to credit card model

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,13 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
+
+  validates :user, :holder_name, :last_digits, :expiration_date, presence: true
+  validate :expiration_date_cannot_be_in_the_past
+
+  private
+    def expiration_date_cannot_be_in_the_past
+      if expiration_date.present? && expiration_date < Date.today
+        errors.add(:expiration_date, "can't be in the past")
+      end
+    end
 end
