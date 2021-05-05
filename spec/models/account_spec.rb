e847ad5d7dcf7ef91956fe3ccf5429290f743ef7
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 15:23:41 -0300

    added test to validate presence of user and default balance value of account model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+
+  subject { build(:account) }
+
   describe 'associations' do
     it { should belong_to(:user)  }
     it { should have_many(:transactions).with_foreign_key('from_account_id') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user) }
+  end
+
+  describe 'default values' do
+    context 'balance' do
+      it 'should be 0.0' do
+        subject.save
+        expect(subject.balance).to eq(0.0)
+      end
+    end
   end
 end
