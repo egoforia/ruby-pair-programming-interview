40f143502d562f258d972a958fa3dbd8dc01aa3e
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 16:24:19 -0300

    updated credit card factory with faker values

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :credit_card do
-    last_digits { "MyString" }
-    expiration_date { "2021-05-05" }
-    holder_name { "MyString" }
-    user { nil }
+    last_digits { Faker::Finance.credit_card.last(4) }
+    expiration_date { Faker::Date.between(from: Date.today, to: 5.years.from_now) }
+    holder_name { Faker::Name.name }
+    user { build(:user) }
   end
 end
