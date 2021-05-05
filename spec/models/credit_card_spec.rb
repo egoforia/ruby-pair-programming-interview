ec7ef90e2c5ef1229fb8dd36efadc20f8d9f0967
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 16:24:47 -0300

    added associations and validations specs for credit card model

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe CreditCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:last_digits) }
+    it { should validate_presence_of(:holder_name) }
+    it { should validate_presence_of(:expiration_date) }
+
+    context 'invalid expiration date' do
+      let(:expiration_date) { Date.yesterday }
+
+      subject { build(:credit_card, expiration_date: expiration_date) }
+      
+      it "should't be in the past" do
+        expect(subject).to_not be_valid
+        expect(subject.errors.messages[:expiration_date]).to include("can't be in the past")
+      end
+    end
+  end
 end
