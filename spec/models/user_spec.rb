4eb848b60b681c011004bc1be536ceb908d8fc72
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 16:25:15 -0300

    added spec to validate credit card association of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many(:accounts) }
+    it { should have_many(:credit_cards) }
   end
 
   describe 'validations' do
