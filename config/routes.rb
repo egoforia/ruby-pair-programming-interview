5cef3ee754cf5cf72c43c4444845b7eedfa5c639
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 15:29:42 -0300

    updated transacations routes to have only index, create and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :transactions
+  resources :transactions, only: [:index, :create, :show]
   resources :accounts, only: [:index, :create, :show]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
