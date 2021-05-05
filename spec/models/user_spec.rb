e023a6d0988893bfe6e03b853310d94f51ee1a4e
Diego Haas Sanches <egoforia@gmail.com>
2021-05-05 14:21:20 -0300

    added specs verify first_name & last_name presences

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many(:accounts) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }  
+  end
 end
