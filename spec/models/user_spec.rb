9465008ee0e35cd57b5a39f8793d693420ab46ea
Diego Haas Sanches <egoforia@gmail.com>
2021-05-06 18:58:07 -0300

    spec to test method balance_total of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }  
   end
+
+  describe 'balance_total' do
+    let(:user) { create(:user) }
+
+    before(:each) do
+      create(:account_with_balance, balance: 55.55, user: user)
+      create(:account_with_balance, balance: 99.99, user: user)
+
+      user.reload
+    end
+
+    it 'should return sum of accounts balance' do
+      expect(user.balance_total).to eq(55.55 + 99.99)
+    end
+  end
 end
