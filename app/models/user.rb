96a082877f5a612f5971707c4569dd752ae3d67b
Diego Haas Sanches <egoforia@gmail.com>
2021-05-06 18:58:50 -0300

    added method balance_total to sum balance of all user's accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :accounts
   has_many :credit_cards
   validates :first_name, :last_name, presence: true
+
+  def balance_total
+    self.accounts.sum(:balance)
+  end
 end
