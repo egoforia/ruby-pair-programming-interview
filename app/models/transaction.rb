d6f06c847500da7d590e71a76ee6421de3a08370
Diego Haas Sanches <egoforia@gmail.com>
2021-05-06 18:05:33 -0300

    added belongs_to credit card association and presence validation of credit card to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :from_account, class_name: 'Account', optional: true
   belongs_to :to_account, class_name: 'Account'
+  belongs_to :credit_card, optional: true
+
+  validates_presence_of :credit_card, if: :credit_card?
 
   enum status: %i[pending success error], _default: "pending"
   enum transaction_type: %i[transfer credit_card], _default: "transfer"
