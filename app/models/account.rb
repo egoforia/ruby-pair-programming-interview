414f94e8cdcd9cd5dea826f89ed64a28db03defe
Diego Haas Sanches <egoforia@gmail.com>
2021-05-06 21:55:13 -0300

    created methods withdraw, deposit and transfer on account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,21 @@ class Account < ApplicationRecord
   has_many :transactions, foreign_key: 'from_account_id'
 
   validates :user, presence: true
+
+  def withdraw(amount)
+    self.update_attribute(:balance, self.balance - amount)
+  end
+
+  def deposit(amount)
+    self.update_attribute(:balance, self.balance + amount)
+  end
+
+  def transfer(amount, to_account)
+    if self.balance >= amount
+      self.withdraw(amount)
+      to_account.deposit(amount)
+    else 
+      raise 'Not enough balance'
+    end
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -21,4 +21,52 @@ RSpec.describe Account, type: :model do
       end
     end
   end
+
+  describe 'withdraw' do
+    subject { create(:account, balance: 100.0) }
+    let(:amount) { 33.33 }
+
+    it 'should remove amount from balance' do
+      expect{ subject.withdraw(amount) }.to change(subject, :balance).by(-amount)
+    end
+  end
+
+  describe 'deposit' do
+    subject { create(:account, balance: 100.0) }
+    let(:amount) { 33.33 }
+    
+    it 'should remove amount from balance' do
+      expect{ subject.deposit(amount) }.to change(subject, :balance).by(amount)
+    end
+  end
+
+  describe 'transfer' do
+    subject { create(:account, balance: 100.0) }
+    let(:to_account) { create(:account) }
+
+    context 'has enough balance' do
+      let(:amount) { 33.33 }
+
+      it 'should transfer amount from account to another acount' do
+        subject.transfer(amount, to_account)
+
+        expect{ subject.transfer(amount, to_account) }.to change(subject, :balance).by(-amount)
+        expect{ subject.transfer(amount, to_account) }.to change(to_account, :balance).by(amount)
+      end
+
+      # it 'should transfer amount from account to another acount' do
+      #   subject.transfer(amount, to_account)
+
+      #   expect{ subject.transfer(amount, to_account) }.to change(to_account, :balance).by(amount)
+      # end
+    end
+
+    context "don't have enough balance" do
+      let(:amount) { 333.33 }
+      
+      it 'should raise Not enough balance error' do
+        expect{ subject.transfer(amount, to_account) }.to raise_error('Not enough balance')
+      end
+    end
+  end
 end
